2fbb6c4c8456c6ca2b70b75260d7bca9b5326592
Rohit <r4rohit002@gmail.com>
2021-01-09 13:18:52 -0700

    Added Binary Tree definition

---
+++ src/Tree/BinaryTree.java
@@ -0,0 +1,37 @@
+package Tree;
+
+public class BinaryTree {
+
+    private int data;
+    private BinaryTree left,right;
+
+    public BinaryTree(int data){
+        this.data = data;
+        left = null;
+        right = null;
+    }
+
+    public int getData(){
+        return data;
+    }
+
+    public BinaryTree getLeft() {
+        return left;
+    }
+
+    public BinaryTree getRight() {
+        return right;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public void setLeft(BinaryTree left) {
+        this.left = left;
+    }
+
+    public void setRight(BinaryTree right) {
+        this.right = right;
+    }
+}
