1d116c9a2a7c4879ed0c05942efa769aac5b3515
Rohit <r4rohit002@gmail.com>
2021-01-11 23:26:01 -0700

    Added quicksort

--- src/Sorting/QuickSort.java
+++ src/Sorting/QuickSort.java
@@ -7,48 +7,77 @@ public class QuickSort {
 
     public static void main(String[] args) {
         QuickSort quickSort = new QuickSort();
-        int[] arr = new int[]{1,4,3,6,5,7,9,3};
-        int[] sortedArray = quickSort.sort(arr, 0, arr.length-1);
+        int[] arr = new int[]{1,4,3,6,5,7,9,8};
+        quickSort.kOrderNum(arr,2);
+
         for(int element: arr){
             System.out.print(element+" ");
         }
     }
 
-
     public int[] sort(int[] arr, int start, int end){
-
-        System.out.println(start+" "+end);
 
         if(start<end){
             int pivot = pivot(arr, start, end);
-            System.out.println("Pivot is "+pivot);
-            sort(arr,0,pivot-1);
+            sort(arr, start, pivot-1);
             sort(arr, pivot+1, end);
         }
 
-
         return arr;
-
     }
 
+    /**
+     * In this function we select a pivot => in Laymen terms we first find the position of an element in the array
+     * How do we find the posion of an element in an array?
+     *      Simple let's say the element is K all the numbers <k will be on the left side and all the element >K will
+     *      be on the right side.
+     * This function returns the position of the element in the array
+     */
     public int pivot(int[] arr, int start, int end){
 
-        int pivotElement = arr[end];
-        int index = start;
+        int pivot = arr[end];
+        int pivotIndex = start;
 
-        for(int i=start;i<end-1;i++){
-            if(arr[i]<=pivotElement){
+        for(int i = 0;i<end;i++){
+            if(arr[i]<pivot){
                 int temp = arr[i];
-                arr[i] = arr[index];
-                arr[index] = temp;
-                index++;
+                arr[i] = arr[pivotIndex];
+                arr[pivotIndex] = temp;
+                pivotIndex++;
             }
         }
 
-        int temp = arr[index];
-        arr[index] = pivotElement;
-        arr[end] = temp;
+        if(arr[pivotIndex]>pivot){
+            int temp = pivot;
+            arr[end] = arr[pivotIndex];
+            arr[pivotIndex] = temp;
+        }
 
-        return index;
+
+        return pivotIndex;
+
     }
+
+
+    //find the k smallest number in an array
+    //Todo for some reason the loop does not stop
+    //Fix it
+    public int kOrderNum(int[] arr, int k){
+
+        int pivot = arr.length;
+
+        while (pivot!=k){
+            System.out.println(pivot);
+            if(k<pivot){
+                pivot = pivot(arr, 0, pivot-1);
+            }else{
+                pivot = pivot(arr, pivot+1, k);
+            }
+        }
+
+        return arr[pivot];
+
+    }
+
+
 }
